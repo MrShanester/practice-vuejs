0682ccc248e56d058dca7d574e4eebf0bf53f9e2
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-05 11:53:15 -0700

    adds task creation button

--- todos.js
+++ todos.js
@@ -1,30 +1,41 @@
-/* global Vue */
+/* global Vue, axios */
 
 var app = new Vue({
   el: "#app",
   data: function () {
     return {
-      message: "Hello from JavaScript!",
-      todos: [
-        {
-          userId: 1,
-          id: 1,
-          title: "Feed Dogs",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "Take out humanoid shaped garbage bags",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: "Clean porch",
-          completed: false,
-        },
-      ],
+      message: "You look really far behind!",
+      todos: [],
+      userId: 0,
+      title: "",
+      completed: false,
+      message2: "",
     };
+  },
+  methods: {
+    loadnames: function () {
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      });
+    },
+    maketodo: function () {
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          userId: this.userId,
+          title: this.title,
+          completed: false,
+        })
+        .then((response) => {
+          console.log(response);
+          this.todos.push(response.data);
+          this.message2 = "Task " + response.data.title + " created.";
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      this.userId = 0;
+      this.title = "";
+    },
   },
 });
